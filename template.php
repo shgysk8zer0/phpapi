5bafb098e65da362cd7eecb036682e82144f5f4c
Chris Zuber <shgysk8zer0@gmail.com>
2019-05-10 10:16:30 -0700

    Better customization
    - Allow setting templates directory
    - Allow setting template extension (default 'html')
    - Implement HTML escaping
    - New methods: `has`, `set`, `get`, & `remove`
    - Misc. whitespace fixes

--- template.php
+++ template.php
@@ -3,6 +3,8 @@ namespace shgysk8zer0\PHPAPI;
 
 final class Template
 {
+	const CHARSET = 'UTF-8';
+
 	private $_data       = [];
 	private $_fname      = '';
 	private $_contents   = '';
@@ -10,14 +12,16 @@ final class Template
 	private $_suffix     = '';
 	private $_capitalize = false;
 
-	private static $_instances = [];
+	private static $_instances    = [];
+	private static $_template_dir = './';
+	private static $_ext          = 'html';
 
 	/**
 	 * [load description]
 	 * @param  string $fname [description]
 	 * @return self          [description]
 	 */
-	final public function load(string $fname): self
+	final public static function load(string $fname): self
 	{
 		if (! array_key_exists($fname, static::$_instances)) {
 			static::$_instances[$fname] = new self($fname);
@@ -25,96 +29,152 @@ final class Template
 		return static::$_instances[$fname];
 	}
 
+	final public static function setTemplateDir(string $dir): bool
+	{
+		if (@is_dir($dir)) {
+			static::$_template_dir = realpath($dir) . DIRECTORY_SEPARATOR;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	final public static function setExtension(string $ext)
+	{
+		static::$_ext = $ext;
+	}
+
 	/**
-	* [__construct description]
-	* @param string  $fname            [description]
-	* @param string  $prefix           [description]
-	* @param string  $suffix           [description]
-	* @param boolean $capitalize       [description]
-	* @param boolean $use_include_path [description]
-	*/
+	 * [__construct description]
+	 * @param string  $fname            [description]
+	 * @param string  $prefix           [description]
+	 * @param string  $suffix           [description]
+	 * @param boolean $capitalize       [description]
+	 * @param boolean $use_include_path [description]
+	 */
 	final public function __construct(
 		string $fname,
 		string $prefix           = '{{',
 		string $suffix           = '}}',
-		bool   $capitalize       = false,
-		bool   $use_include_path = true
+		bool   $capitalize       = false
 	)
 	{
 		$this->_fname      = $fname;
 		$this->_prefix     = $prefix;
 		$this->_suffix     = $suffix;
 		$this->_capitalize = $capitalize;
-		$this->_contents   = file_get_contents($fname, $use_include_path);
+		$this->_contents   = file_get_contents(static::$_template_dir . "{$fname}." . static::$_ext);
 	}
 
 	/**
-	* [__get description]
-	* @param  string $key [description]
-	* @return [type]      [description]
-	*/
+	 * [__get description]
+	 * @param  string $key [description]
+	 * @return [type]      [description]
+	 */
 	final public function __get(string $key): string
+	{
+		return $this->get($key);
+	}
+
+	/**
+	 * [__set description]
+	 * @param string $key   [description]
+	 * @param string $value [description]
+	 */
+	final public function __set(string $key, string $value)
+	{
+		$this->set($key, $value);
+	}
+
+	/**
+	 * [__isset description]
+	 * @param  string  $key [description]
+	 * @return boolean      [description]
+	 */
+	final public function __isset(string $key): bool
+	{
+		return $this->has($key);
+	}
+
+	/**
+	 * [__unset description]
+	 * @param string $key [description]
+	 */
+	final public function __unset(string $key)
+	{
+		$this->remove($key);
+	}
+
+	final public function __call(string $key, array $values): self
+	{
+		$this->__set($key, $values[0]);
+		return $this;
+	}
+
+	/**
+	 * [__toString description]
+	 * @return string [description]
+	 */
+	final public function __toString(): string
+	{
+		return str_replace(
+			array_keys($this->_data),
+			array_values($this->_data),
+			$this->_contents
+		);
+	}
+
+	/**
+	 * [__debugInfo description]
+	 * @return array [description]
+	 */
+	final public function __debugInfo(): array
+	{
+		return [
+			'fname'      => $this->_fname,
+			'directory'  => static::$_template_dir,
+			'extension'  => static::$_ext,
+			'prefix'     => $this->_prefix,
+			'suffix'     => $this->_suffix,
+			'capitalize' => $this->_capitalize,
+			'data'       => $this->_data,
+		];
+	}
+
+	final public function get(string $key): string
 	{
 		return $this->_data[$this->_convertKey($key)] ?? '';
 	}
 
-	/**
-	* [__set description]
-	* @param string $key   [description]
-	* @param string $value [description]
-	*/
-	final public function __set(string $key, string $value)
+	final public function set(
+		string $key,
+		string $value,
+		bool   $escape  = true,
+		string $charset = self::CHARSET
+	): self
 	{
-		$this->_data[$this->_convertKey($key)] = $value;
+		if ($escape) {
+			$this->_data[$this->_convertKey($key)] = htmlspecialchars($value, ENT_COMPAT | ENT_HTML5, $charset);
+		} else {
+			$this->_data[$this->_convertKey($key)] = $value;
+		}
+		return $this;
 	}
 
-	/**
-	* [__isset description]
-	* @param  string  $key [description]
-	* @return boolean      [description]
-	*/
-	final public function __isset(string $key): bool
+	final public function has(string $key): bool
 	{
 		return array_key_exists($this->_convertKey($key), $this->_data);
 	}
 
-	/**
-	* [__unset description]
-	* @param string $key [description]
-	*/
-	final public function __unset(string $key)
+	final public function remove(string $key)
 	{
 		unset($this->_data[$this->_convertKey($key)]);
 	}
 
 	/**
-	* [__toString description]
-	* @return string [description]
-	*/
-	final public function __toString(): string
-	{
-		return str_replace(array_keys($this->_data), array_values($this->_data), $this->_contents);
-	}
-
-	/**
-	* [__debugInfo description]
-	* @return array [description]
-	*/
-	final public function __debugInfo(): array
-	{
-	return [
-		'fname'      => $this->_fname,
-		'prefix'     => $this->_prefix,
-		'suffix'     => $this->_suffix,
-		'capitalize' => $this->_capitalize,
-		'data'       => $this->_data,
-	];
-	}
-
-	/**
-	* [reset description]
-	* @return bool [description]
-	*/
+	 * [reset description]
+	 * @return bool [description]
+	 */
 	final public function reset(): bool
 	{
 		$empty = empty($this->_data);
@@ -123,13 +183,13 @@ final class Template
 	}
 
 	/**
-	* [_convertKey description]
-	* @param  string $key [description]
-	* @return string      [description]
-	*/
+	 * [_convertKey description]
+	 * @param  string $key [description]
+	 * @return string      [description]
+	 */
 	final private function _convertKey(string $key): string
 	{
-	return $this->_capitalize ? strtoupper("{$this->_prefix}{$key}{$this->_suffix}")
-		: "{$this->_prefix}{$key}{$this->_suffix}";
+		return $this->_capitalize ? strtoupper("{$this->_prefix}{$key}{$this->_suffix}")
+			: "{$this->_prefix}{$key}{$this->_suffix}";
 	}
-	}
+}
