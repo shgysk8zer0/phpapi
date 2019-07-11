0fff87a41e238764cab3e861c84565ba4f0b2f42
Chris Zuber <shgysk8zer0@gmail.com>
2019-07-11 11:09:14 -0700

    Update registration
    - Add interface for `InputData`
    - Implement in `GetData` & `PostData`
    - Add trait for `InputData`
    - Update `User::create` to work with `InputData $input`
    - Create `Person` & `users` in transation
    - `PDO::commit` or `PDO::rollback` on success / error

--- postdata.php
+++ postdata.php
@@ -1,7 +1,7 @@
 <?php
 namespace shgysk8zer0\PHPAPI;
 
-class PostData implements \JSONSerializable, \Iterator
+class PostData implements \JSONSerializable, \Iterator, Interfaces\InputData
 {
 	use Traits\Singleton;
 	use Traits\InputData;
