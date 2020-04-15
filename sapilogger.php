d825f9520eb4d11324df96aa68a7b0927b07292c
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-15 12:58:17 -0700

    Make SAPILogger only work on CLI
    Only log if `PHP_SAPI` is CLI or CLI Server

--- sapilogger.php
+++ sapilogger.php
@@ -5,13 +5,15 @@ use \InvalidArgumentException;
 
 class SAPILogger extends Abstracts\AbstractLogger
 {
+	public const SAPI_CLIENTS = ['cli', 'cli-server'];
+
 	use Traits\LoggerInterpolatorTrait;
 
 	final public function log(string $level, string $message, array $context = []): void
 	{
 		if (! in_array($level, Abstracts\LogLevel::ALL_LEVELS)) {
 			throw new InvalidArgumentException(sprintf('Invalid log level: "%s"', $level));
-		} else {
+		} elseif (in_array(PHP_SAPI, self::SAPI_CLIENTS)) {
 			$msg = sprintf('[%s] "%s"', $level, $this->interpolate($message, $context));
 			error_log($msg, 4);
 		}
