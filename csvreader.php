dc57fd6ddc2279a00e0c02087eb982ffc863835a
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-23 10:29:01 -0700

    Implement WIP writing

--- csvreader.php
+++ csvreader.php
@@ -3,6 +3,7 @@ namespace shgysk8zer0\PHPAPI;
 
 use \shgysk8zer0\PHPAPI\Interfaces\{LoggerAwareInterface};
 use \shgysk8zer0\PHPAPI\Traits\{LoggerAwareTrait};
+use \shgysk8zer0\PHPAPI\Abstracts\{LogLevel};
 use \InvalidArgumentException;
 use \Throwable;
 use \RuntimeException;
@@ -12,7 +13,6 @@ class CSVReader implements LoggerAwareInterface
 	use LoggerAwareTrait;
 
 	private $_handle = null;
-	private $_filename = null;
 
 	public function __construct()
 	{
@@ -21,11 +21,7 @@ class CSVReader implements LoggerAwareInterface
 
 	public function __destruct()
 	{
-		if (is_resource($this->_handle)) {
-			flock($this->_handle, LOCK_UN);
-			fclose($this->_handle);
-			$this->_handle = null;
-		}
+		$this->_close();
 	}
 
 	public function __toString(): string
@@ -46,6 +42,8 @@ class CSVReader implements LoggerAwareInterface
 
 	public function open(string $fname, string $mode = 'r'): bool
 	{
+		$this->close();
+
 		try {
 			if (! file_exists($fname)) {
 				throw new InvalidArgumentException("{$fname} not found");
@@ -59,15 +57,63 @@ class CSVReader implements LoggerAwareInterface
 				return true;
 			}
 		} catch (Throwable $e) {
-			$this->logger->error('[{type} {code}] "{message}" at {file}:{line}', [
-				'type'    => get_class($e),
-				'code'    => $e->getCode(),
-				'message' => $e->getMessage(),
-				'file'    => $e->getFile(),
-				'line'    => $e->getLine(),
-			]);
+			$this->_logException($e);
 
 			return false;
+		}
+	}
+
+	public function close(): bool
+	{
+		if (is_resource($this->_handle)) {
+			flock($this->_handle, LOCK_UN);
+			fclose($this->_handle);
+			$this->_handle = null;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function write(string $fname, iterable $rows): bool
+	{
+		$handle  = null;
+		$tmp     = null;
+		$ret_val = true;
+
+		try {
+			if (! $handle = fopen($fname, 'w+')) {
+				throw new RuntimeException("Unable to open {$fname} for writing");
+			} elseif (! flock($handle, LOCK_EX)) {
+				throw new RuntimeException("Unable to acquire lock on {$fname}");
+			} else {
+				$tmp = tmpfile();
+				flock($tmp, LOCK_EX);
+
+				foreach ($rows as $row) {
+					if (fputcsv($tmp, $row) === false) {
+						throw new RuntimeException("Unable to write row to {$fname}");
+					}
+				}
+
+				rewind($tmp);
+				return stream_copy_to_stream($tmp, $handle) !== false;
+			}
+		} catch (Throwable $e) {
+			$this->_logException($e);
+			$ret_val = false;
+		} finally {
+			if (is_resource($handle)) {
+				flock($handle, LOCK_UN);
+				fclose($handle);
+			}
+
+			if (is_resource($tmp)) {
+				flock($tmp, LOCK_UN);
+				fclose($tmp);
+			}
+
+			return $ret_val;
 		}
 	}
 
@@ -91,5 +137,16 @@ class CSVReader implements LoggerAwareInterface
 		} else {
 			return null;
 		}
+	}
+
+	private function _logException(Throwable $e, string $level = LogLevel::ERROR): void
+	{
+		$this->logger->log($level, '[{type} {code}] "{message}" at {file}:{line}', [
+			'type'    => get_class($e),
+			'code'    => $e->getCode(),
+			'message' => $e->getMessage(),
+			'file'    => $e->getFile(),
+			'line'    => $e->getLine(),
+		]);
 	}
 }
