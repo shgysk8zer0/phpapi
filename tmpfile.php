f6d74b3bb1ef1341122ef9ccce2f1408a88f06f7
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-23 13:07:38 -0700

    Add TmpFile class
    Implementing FileUtilsInterface, but it's only a temporary file

---
+++ tmpfile.php
@@ -0,0 +1,40 @@
+<?php
+namespace shgysk8zer0\PHPAPI;
+
+use \shgysk8zer0\PHPAPI\Interfaces\{FileUtilsInterface};
+use \shgysk8zer0\PHPAPI\Traits\{FileUtilsTrait};
+use \RuntimeException;
+
+class TmpFile implements FileUtilsInterface
+{
+	use FileUtilsTrait;
+
+	public function open(): bool
+	{
+		if ($this->isOpen()) {
+			$this->close();
+		}
+		$this->_handle = tmpfile();
+		return $this->isOpen();
+	}
+
+	public function saveAs(string $fname): bool
+	{
+		try {
+			if (! $this->isOpen()) {
+				throw new RuntimeException('Cannot save from an unopened file');
+			}
+			$handle = fopen($fname, 'w');
+			flock($handle, LOCK_EX);
+			$pos = $this->getPosition();
+			$this->rewind();
+			$this->copyTo($handle);
+			flock($handle, LOCK_UN);
+			fclose($handle);
+			$this->seek($pos);
+			return true;
+		} catch (Throwable $e) {
+			return false;
+		}
+	}
+}
